7a6d28752e29e890cd192dfca956713e01cc1230
Isai Rincon <Rinconisai@gmail.com>
2023-05-17 12:14:02 -0700

    Mofe physics adjustments and attempts for timer

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//let timer;
 class Test extends Phaser.Scene{
     constructor(){
         super('test')
@@ -6,20 +7,31 @@ class Test extends Phaser.Scene{
 
     }
     create(){
-       let b = this.add.circle(300, 400, 30, 0xfffff);
+       // timer = 0;
+       this.l1_time = new Phaser.Time.TimerEvent()
+       let b = this.add.circle(300, 700, 35, 0xfffff);
        let ball_obj = this.physics.add.existing(b, 0);
        this.ball = ball_obj.body;
        this.ball.setCollideWorldBounds(true);
-       this.ball.setBounce(1);
-       this.ball.setVelocity(150);
+       this.ball.setBounce(1.05);
+       this.ball.setVelocity(250);
        this.ball.onOverlap = true;
+       this.ball.onCollide = true;
 
-       let p = this.add.rectangle(0, this.game.config.height, 150, 200).setFillStyle(0xFFFFF);
-       this.l = this.add.rectangle(75, 25, 120, 35).setFillStyle(0xFFFFFF);
+       let g = this.add.circle(this.game.config.width-200, 250, 150, 0xffffff);
+       let g_obj = this.physics.add.existing(g, 0);
+       this.goal = g_obj.body;
+       this.goal.immovable = true;
+       this.goal.allowGravity = false;
+       this.goal.onOverlap = true;
+
+       let p = this.add.rectangle(0, this.game.config.height, 150, 200).setFillStyle(0xff0000);
+       this.l = this.add.rectangle(210, this.game.config.height+75, 120, 35).setFillStyle(0x000000);
        let player_obj = this.physics.add.existing(p, 0);
        let leg_obj = this.physics.add.existing(this.l, 0);
        this.player = player_obj.body;
        this.player.onOverlap = true;
+       this.player.onCollide = true;
 
        this.leg = leg_obj.body;
        this.player.setBounce(0.2);
@@ -28,37 +40,25 @@ class Test extends Phaser.Scene{
        this.leg.allowGravity = false;
        this.leg.immovable = true;
        this.leg.onOverlap = true;
+       this.leg.onCollide = true;
        
        this.physics.add.collider(this.ball, this.player);
        this.physics.add.collider(this.ball, this.leg);
 
        this.cursors = this.input.keyboard.createCursorKeys();
-       this.pause = false;
-       this.physics.add.overlap(this.ball, this.player);
-       this.physics.add.overlap(this.ball, this.leg);
-
-       this.physics.world.on('overlap', () =>{
-        this.ball.setVelocityY(this.player.Velocity)
-       })
+       this.physics.add.overlap(this.ball, this.goal);
     }
     update(){
-        // this.input.keyboard.on('keyup-'+'P', () => {
-        //     this.pause = !this.pause;
-        //     console.log("changed pause");
-        // })
-        // // if(this.cursors.p.isDown){
-        // //     this.pause = !this.pause;
-        // // }
-        // if(this.pause){
-        //     //game.scene.pause('test');
-        //     this.scene.pause('test');
-        // }
-        // this.scene.launch('test');
-        //let cursors = this.input.keyboard.createCursorKeys();
-        //const {left, right, spacebar} = this.cursors;
-        this.leg.x = this.player.x+155;
+       // timer++;
+        this.physics.world.on('collide', () =>{
+            this.ball.setVelocityY(-555);
+            this.ball.setBounce(1.1)
+        })
+        this.physics.world.on('overlap', () =>{
+            this.scene.start('l1_done', this.l1_time.getElapsedSeconds());
+        })
+        
         this.leg.y = this.player.y+165;
-        let jump_counter = 0;
 
         if(this.cursors.left.isDown){
            this.player.setVelocityX(-400);
@@ -72,18 +72,59 @@ class Test extends Phaser.Scene{
             this.player.setVelocityX(0);
         }
 
-        if(this.cursors.space.isDown){ //&& this.player.touching.down){
-            jump_counter++;
-            console.log(jump_counter);
-            if(jump_counter == 1){
-                this.player.setVelocityY(-200);
-            }
-            if(jump_counter > 1){
-                this.player.setVelocityY(-400);
-            }
-
-        }   
+        if(this.cursors.space.isDown && this.player.onFloor()){
+          this.player.setVelocityY(-400);
+        } 
+        this.input.keyboard.on('keydown-'+'R',()=>{
+            this.scene.start('test');
+        })
+        
     }
+}
+
+
+class Level1_Done extends Phaser.Scene{
+    constructor(){
+        super('l1_done')
+    }
+    preload(){
+
+    }
+    //figure out this business
+    init(data){
+        console.log('init', data);
+        this.seconds = data;
+    }
+    create(){
+        this.add.text(150, 300, "Time: "+ this.seconds + " seconds");
+        this.g = this.add.circle(this.game.config.width-200, this.game.config.height-200, 50, 0x000000);
+        this.b = this.add.circle(300, this.game.config.height-200, 30, 0xfffff);
+        
+        this.b.setInteractive({draggable: true});
+        //code below taken from https://labs.phaser.io/edit.html?src=src/input/dragging/drag%20horizontally.js
+        //~~~~~~~~
+        this.input.on('drag', (pointer, gameObject, dragX) => {
+
+            //  By clamping dragX we can keep it within
+            //  whatever bounds we need
+            dragX = Phaser.Math.Clamp(dragX, 100, this.game.config.width-200);
+
+            //  By only applying the dragX we can limit the drag
+            //  to be horizontal only
+            gameObject.x = dragX;
+
+        });
+        //~~~~~~~~~
+
+
+    }
+
+    update(){
+        if(this.b.x == this.g.x){
+            this.scene.start('test');
+        }
+    }
+
 }
 
 const game = new Phaser.Game({
@@ -103,7 +144,7 @@ const game = new Phaser.Game({
              }
         }
     },
-    scene: [Test],
+    scene: [Test, Level1_Done],
     title: "Physics Demo",
     backgroundColor: "0x228b22"
 });
