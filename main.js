a21ef62525ccf34030bb6fc55962eeb24bac7e6b
Isai Rincon <Rinconisai@gmail.com>
2023-05-17 15:28:42 -0700

    Level 1 Tweaking and more levels

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-//let timer;
-class Test extends Phaser.Scene{
+class Level1 extends Phaser.Scene{
     constructor(){
-        super('test')
+        super('l1')
     }
     preload(){
 
     }
     create(){
-       // timer = 0;
-       this.l1_time = new Phaser.Time.TimerEvent()
        let b = this.add.circle(300, 700, 35, 0xfffff);
        let ball_obj = this.physics.add.existing(b, 0);
        this.ball = ball_obj.body;
@@ -42,20 +39,34 @@ class Test extends Phaser.Scene{
        this.leg.onOverlap = true;
        this.leg.onCollide = true;
        
-       this.physics.add.collider(this.ball, this.player);
-       this.physics.add.collider(this.ball, this.leg);
+       this.physics.add.collider(
+        this.ball, 
+        this.player,
+        null,
+        (ball, player)=>{
+            this.touches++;
+        }
+       );
+
+       this.physics.add.collider(
+        this.ball, 
+        this.leg,
+        null,
+        (ball, leg)=>{
+            this.touches++;
+        });
 
        this.cursors = this.input.keyboard.createCursorKeys();
        this.physics.add.overlap(this.ball, this.goal);
+       this.touches = 0;
     }
     update(){
-       // timer++;
         this.physics.world.on('collide', () =>{
             this.ball.setVelocityY(-555);
             this.ball.setBounce(1.1)
         })
         this.physics.world.on('overlap', () =>{
-            this.scene.start('l1_done', this.l1_time.getElapsedSeconds());
+            this.scene.start('l1_done', {touches: this.touches});
         })
         
         this.leg.y = this.player.y+165;
@@ -90,13 +101,14 @@ class Level1_Done extends Phaser.Scene{
     preload(){
 
     }
-    //figure out this business
     init(data){
-        console.log('init', data);
-        this.seconds = data;
+        this.touches = data.touches;
     }
     create(){
-        this.add.text(150, 300, "Time: "+ this.seconds + " seconds");
+        this.add.text(300, 400,"Number of Touches: " +this.touches).setFontSize(35);
+        this.add.text(this.game.config.width/3, 200, "LEVEL 1 COMPLETED!").setFontSize(65);
+        this.add.text(300, 300, "You’re doing great, kid! A recruiter even noticed your skills. Show them what you got!!").setWordWrapWidth(1500).setFontSize(45);
+        this.add.text(300, 1000, "[Slide the ball to the left to keep going!]").setFontSize(35)
         this.g = this.add.circle(this.game.config.width-200, this.game.config.height-200, 50, 0x000000);
         this.b = this.add.circle(300, this.game.config.height-200, 30, 0xfffff);
         
@@ -115,16 +127,115 @@ class Level1_Done extends Phaser.Scene{
 
         });
         //~~~~~~~~~
-
-
     }
 
     update(){
         if(this.b.x == this.g.x){
-            this.scene.start('test');
+            this.scene.start('l2');
         }
     }
 
+}
+
+class Level2 extends Phaser.Scene{
+    constructor(){
+        super('l2');
+    }
+    preload(){
+
+    }
+    create(){
+       let b = this.add.circle(300, 700, 35, 0xfffff);
+       let ball_obj = this.physics.add.existing(b, 0);
+       this.ball = ball_obj.body;
+       this.ball.setCollideWorldBounds(true);
+       this.ball.setBounce(1.05);
+       this.ball.setVelocity(250);
+       this.ball.onOverlap = true;
+       this.ball.onCollide = true;
+
+       let g = this.add.circle(this.game.config.width-200, 250, 150, 0xffffff);
+       let g_obj = this.physics.add.existing(g, 0);
+       this.goal = g_obj.body;
+       this.goal.immovable = true;
+       this.goal.allowGravity = false;
+       this.goal.onOverlap = true;
+
+       let p = this.add.rectangle(0, this.game.config.height, 150, 200).setFillStyle(0xff0000);
+       this.l = this.add.rectangle(210, this.game.config.height+75, 120, 35).setFillStyle(0x000000);
+       let player_obj = this.physics.add.existing(p, 0);
+       let leg_obj = this.physics.add.existing(this.l, 0);
+       this.player = player_obj.body;
+       this.player.onOverlap = true;
+       this.player.onCollide = true;
+
+       this.leg = leg_obj.body;
+       this.player.setBounce(0.2);
+       this.player.setCollideWorldBounds(true);
+       this.leg.setCollideWorldBounds(true);
+       this.leg.allowGravity = false;
+       this.leg.immovable = true;
+       this.leg.onOverlap = true;
+       this.leg.onCollide = true;
+       
+       //makin cones
+       let c1 = this.add.triangle(500, this.game.config.height-50, 0, 128, 128, 128, 74, 0, 0xffa500);
+       let c2 = this.add.triangle(700, this.game.config.height-50, 0, 128, 128, 128, 74, 0, 0xffa500);
+       let c3 = this.add.triangle(900, this.game.config.height-50, 0, 128, 128, 128, 74, 0, 0xffa500);
+       //ADD PHYSICS TO CONES!!
+
+       this.physics.add.collider(
+        this.ball, 
+        this.player,
+        null,
+        (ball, player)=>{
+            this.touches++;
+        }
+       );
+
+       this.physics.add.collider(
+        this.ball, 
+        this.leg,
+        null,
+        (ball, leg)=>{
+            this.touches++;
+        });
+
+       this.cursors = this.input.keyboard.createCursorKeys();
+       this.physics.add.overlap(this.ball, this.goal);
+       this.touches = 0;
+    }
+    update(){
+        this.physics.world.on('collide', () =>{
+            this.ball.setVelocityY(-555);
+            this.ball.setBounce(1.1)
+        })
+        this.physics.world.on('overlap', () =>{
+            this.scene.start('l1_done', {touches: this.touches});
+        })
+        
+        this.leg.y = this.player.y+165;
+
+        if(this.cursors.left.isDown){
+           this.player.setVelocityX(-400);
+           this.leg.x = this.player.x-125;
+        }
+        else if(this.cursors.right.isDown){
+            this.player.setVelocityX(400);
+            this.leg.x = this.player.x+155;
+        }
+        else{
+            this.player.setVelocityX(0);
+        }
+
+        if(this.cursors.space.isDown && this.player.onFloor()){
+          this.player.setVelocityY(-400);
+        } 
+        this.input.keyboard.on('keydown-'+'R',()=>{
+            this.scene.start('l2');
+        })
+        
+    }
 }
 
 const game = new Phaser.Game({
@@ -144,7 +255,7 @@ const game = new Phaser.Game({
              }
         }
     },
-    scene: [Test, Level1_Done],
+    scene: [Level1, Level1_Done, Level2],
     title: "Physics Demo",
     backgroundColor: "0x228b22"
 });
